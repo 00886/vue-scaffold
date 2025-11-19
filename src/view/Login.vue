0cd2b1bae01fbd220e9a0fc562fd6f82c497789e
00886 <175072975@qq.com>
2025-11-20 00:19:50 +0800

    监听用户名和密码，动态设置登录按钮禁用状态

--- src/view/Login.vue
+++ src/view/Login.vue
@@ -1,27 +1,52 @@
 <script setup>
-import { reactive } from 'vue'
-import { User,Lock } from '@element-plus/icons-vue'
+import { reactive, ref, watch, watchEffect } from 'vue'
+import { User, Lock } from '@element-plus/icons-vue'
 
 const loginInfo = reactive({
     username: '',
     password: ''
-});
+})
+
+const loginRef = ref()
+const rules = reactive({
+    username: [
+        { required: true, message: '请输入用户名', trigger: 'blur' },
+        { min: 1, max: 10, message: '长度在1到10个字符', trigger: 'blur' },
+    ],
+    password: [
+        { required: true, message: '请输入密码', trigger: 'blur' },
+        { min: 1, max: 10, message: '长度在1到10个字符', trigger: 'blur' },
+    ],
+})
+
+const loginButtonDisabled = ref(true)
+
+// 监听用户名和密码的变化，动态设置登录按钮的禁用状态
+watch(
+    [()=>loginInfo.username,()=>loginInfo.password],
+    () => {
+        loginRef.value.validate(valid => {
+            loginButtonDisabled.value = !valid
+        })
+    }
+)
 </script>
 
 <template>
     <el-card style="max-width: 480px" class="box-card">
         <h2>后台管理系统</h2>
 
-        <el-form ref="ruleFormRef" style="max-width: 600px" :model="loginInfo" status-icon :rules="rules"
+        <el-form ref="loginRef" style="max-width: 600px" :model="loginInfo" status-icon :rules="rules"
             label-width="auto" class="demo-ruleForm">
-             <el-form-item label="Username" prop="username">
-                <el-input v-model="loginInfo.username" :prefix-icon="User" clearable/>
+            <el-form-item label="Username" prop="username">
+                <el-input v-model="loginInfo.username" :prefix-icon="User" clearable />
             </el-form-item>
             <el-form-item label="Password" prop="password">
-                <el-input v-model="loginInfo.password" type="password" autocomplete="off" :prefix-icon="Lock" clearable show-password/>
+                <el-input v-model="loginInfo.password" type="password" autocomplete="off" :prefix-icon="Lock" clearable
+                    show-password />
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="submitForm(ruleFormRef)">
+                <el-button type="primary" @click="submitForm(ruleFormRef)" :disabled="loginButtonDisabled">
                     登录
                 </el-button>
             </el-form-item>
