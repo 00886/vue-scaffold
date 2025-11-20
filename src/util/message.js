ad31f8a690b07dea0ca15ed5c685d9a53f5fe72b
00886 <175072975@qq.com>
2025-11-20 11:21:38 +0800

    封装message工具

---
+++ src/util/message.js
@@ -0,0 +1,20 @@
+import { ElMessage } from 'element-plus'
+
+export const showMessage = ({
+    message = '',
+    type = 'info',
+    duration = 1500,
+    showClose = true,
+}) => {
+    ElMessage({
+        message,
+        type,
+        duration,
+        showClose,
+    })
+}
+
+export const showSuccess = (message) => showMessage({ message: message, type: 'success' })
+export const showError = (message) => showMessage({ message: message, type: 'error' })
+export const showWarning = (message) => showMessage({ message: message, type: 'warning' })
+export const showInfo = (message) => showMessage({ message: message, type: 'info' })
