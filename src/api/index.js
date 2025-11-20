ceddfaa33cb4c12f1fa9b06cac2811e2c322d62b
00886 <175072975@qq.com>
2025-11-20 10:28:39 +0800

    请求拦截器添加请求头token

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,25 @@
 import axios from 'axios'
+import { CONFIG } from '../config'
+
+// 请求拦截器
+axios.interceptors.request.use(config => {
+    console.log("请求拦截器：", config)
+    // 在发送请求之前做些什么，比如添加token
+    let timestamp = Date.now()
+    if (config.method === 'get') {
+        if (!config.params) {
+            config.params = {}
+        }
+        config.params.timestamp = timestamp
+    }
+
+    // 添加token到请求头
+    config.headers.set(CONFIG.TOKEN_NAME, window.localStorage.getItem(CONFIG.TOKEN_NAME) || '')
+    return config
+}, error => {
+    // 对请求错误做些什么
+    return Promise.reject(error)
+})
 
 const request = ({ url = "", method = "get", data = {}, timeout = 1000 }) => {
     // console.log("使用封装函数去处理请求")
