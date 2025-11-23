5b9fc546cf4f803706a5bd8d22824e50434b1e41
00886 <175072975@qq.com>
2025-11-24 01:11:11 +0800

    header功能布局和用户退出功能

--- src/view/layout/components/Header.vue
+++ src/view/layout/components/Header.vue
@@ -1,7 +1,51 @@
 <template>
-    <el-header>Header</el-header>
+    <div>
+        <el-header class="el-header">
+            <el-button>
+                <el-icon :size="25"><span class="iconfont icon-appshouqi"></span></el-icon>
+            </el-button>
+            <el-button @click="handleLogout"
+                ><el-icon :size="25"><span class="iconfont icon-dengchutuichuguanbi"></span></el-icon
+            ></el-button>
+        </el-header>
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+import '../../../assets/iconfont/iconfont.css'
+import { CONFIG } from '../../../config/index.js'
+import { useRouter } from 'vue-router'
+import { ElMessageBox } from 'element-plus'
+import { showWarning } from '../../../util/message.js'
 
-<style scoped></style>
+const router = useRouter()
+
+const handleLogout = () => {
+    ElMessageBox.confirm('确认退出吗？', '', {
+        confirmButtonText: '确认',
+        cancelButtonText: '取消',
+        type: 'warning',
+    })
+        .then(() => {
+            // 移除token
+            window.localStorage.removeItem(CONFIG.TOKEN_NAME)
+            showWarning('退出成功')
+            setTimeout(() => {
+                router.replace('/login')
+            }, 1500)
+        })
+        .catch(() => {})
+}
+</script>
+
+<style lang="less" scoped>
+.el-header {
+    height: 50px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    button {
+        border: 0;
+    }
+}
+</style>
