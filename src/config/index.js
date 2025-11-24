38bcfb35aaf3c0b98a2bc8ef3d7aa37cdd2cbc6f
00886 <175072975@qq.com>
2025-11-24 23:03:06 +0800

    多环境配置

--- src/config/index.js
+++ src/config/index.js
@@ -1,13 +1,16 @@
+console.log('获取环境变量：', import.meta.env)
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
 // 用来放项目的配置信息
 export const API_CONFIG = {
-    LoginAPI: 'http://localhost:8888/api/auth/login',
-    LogoutAPI: 'http://localhost:8888/api/auth/logout',
-    userListAPI: 'http://localhost:8888/api/user/list',
-    DelteUserAPI: 'http://localhost:8888/api/user/delete',
-    AddUserAPI: 'http://localhost:8888/api/user/add',
-    UpdateUserAPI: 'http://localhost:8888/api/user/update',
+    LoginAPI: `${BASE_URL}/auth/login`,
+    LogoutAPI: `${BASE_URL}/auth/logout`,
+    userListAPI: `${BASE_URL}/user/list`,
+    DelteUserAPI: `${BASE_URL}/user/delete`,
+    AddUserAPI: `${BASE_URL}/user/add`,
+    UpdateUserAPI: `${BASE_URL}/user/update`,
 }
 
 export const CONFIG = {
-    TOKEN_NAME: 'Authorization',
+    TOKEN_NAME: `Authorization`,
 }
